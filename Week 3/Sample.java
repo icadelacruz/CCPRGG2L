169a5f9e6beee878500b55d259f7b1bc947cb7b1
icadelacruz <82089431+icadelacruz@users.noreply.github.com>
2022-12-12 14:34:10 +0800

    Create Sample.java

---
+++ Week 3/Sample.java
@@ -0,0 +1,14 @@
+public class Sample {
+    public static void main(String[] args) {
+        int x = 0;
+        int y = 5;
+        
+        System.out.println(y/x);
+        // Math error
+
+//        String s = null;
+//        System.out.println(s.length());
+        
+        System.out.println("This is another line in the program");
+    }
+}
